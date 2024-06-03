52df733df9d1676fabc0f5309dbb8bba1d38fe7d
Ramakant Prajapati <121657019+Pram12345@users.noreply.github.com>
2024-06-03 16:55:11 +0530

    Update Filesize.java

--- Filesize.java
+++ Filesize.java
@@ -3,7 +3,7 @@ import java.io.File;
 public class Filesize {
 
     public static void main(String[] args) {
-        String filePath = "example.txt"; // Path to the file
+        String filePath = "example.txt"; 
 
         getFileSize(filePath);
     }
@@ -14,14 +14,13 @@ public class Filesize {
         if (file.exists() && file.isFile()) {
             
             long bytes = file.length();
-            // Convert to kilobytes (1 KB = 1024 Bytes)
+            
             double kilobytes = (double) bytes / 1024;
-            // Convert to megabytes (1 MB = 1024 KB)
-            // double megabytes = kilobytes / 1024;
+         
 
             System.out.println("File size in bytes: " + bytes);
             System.out.printf("File size in kilobytes: %.2f\n", kilobytes);
-            // System.out.printf("File size in megabytes: %.2f\n", megabytes);
+          
         } else {
             System.out.println("File does not exist or is not a valid file.");
         }
